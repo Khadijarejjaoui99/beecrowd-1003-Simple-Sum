e0f0368d33d406fdef371a3a14ee9b506896fe19
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 19:42:27 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,14 @@
+// Solution of 1003 - Simple Sum in beecrowd
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+ 
+    int A,B,SOMA;
+    cin >> A >>B;
+    SOMA = A + B;
+    cout<<"SOMA = " << SOMA <<endl;
+    system("pause");
+    return 0;
+}
